eda9819c1ad3bdfeefd5d37965215b67a3740b12
julieclucas <julieclucas@fullsail.edu>
2013-11-29 20:36:03 -0500

    added winnercheck

--- Week1/goal2_assign_duel2/js/main.js
+++ Week1/goal2_assign_duel2/js/main.js
@@ -35,10 +35,10 @@
 
         for (var i = 0; i < 10; i++){
             //random  formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = player1Damage * .5;
-            var minDamage2 = player2Damage * .5;
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+            var minDamage1 = fighter1[1] * .5;
+            var minDamage2 = fighter2[1] * .5;
+            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(fighter2[1]-minDamage2)+minDamage2);
 
             //inflict damage
             fighter1[2]-=f1;
@@ -65,14 +65,14 @@
 
     function winnerCheck (){
         var result="no winner";
-        if (playerOneHealth<1 && playerTwoHealth<1)
+        if (fighter1[2]<1 && fighter2[2]<1)
         {
             result = "You Both Die";
-        } else if(playerOneHealth<1){
-            result =playerTwoName+" WINS!!!"
-        } else if (playerTwoHealth<1)
+        } else if(fighter1[2]<1){
+            result =fighter2[0]+" WINS!!!"
+        } else if (fighter2[2]<1)
         {
-            result = playerOneName+" WINS!!!"
+            result = fighter2[0]+" WINS!!!"
         };
         return result;
     };
